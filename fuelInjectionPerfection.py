44e9238af725e028188ef0eb4a0a9674f85dc0ea
Steven Hatch <90797071+stevegreghatch@users.noreply.github.com>
2022-07-22 14:55:47 -0400

    Update fuelInjectionPerfection.py

--- fuelInjectionPerfection.py
+++ fuelInjectionPerfection.py
@@ -1,10 +1,3 @@
-# n1 = '15'
-# n1S = 5
-# n2 = '4'
-# n2S = 2
-
-# n = '11'
-
 def solution(n):
     possibleNumbersTotal = []
     possibleNumbers = []
@@ -12,17 +5,14 @@ def solution(n):
     possibleNumbers.append(n)
 
     def function(n):
-        # print('original: ' + str(n))
         while n != 1:
             if n % 2 == 0:
                 n = int(n/2)
                 possibleNumbers.append(n)
-                # print('divided: ' + str(n))
             else:
                 options = []
                 options.append(n+1)
                 options.append(n-1)
-                # print('options: ' + str(options))
                 for i in range(len(options)):
                     if i == 0:
                         possibleNumbers.append(n+1)
@@ -32,24 +22,20 @@ def solution(n):
                     function(options[i])
                     
                     if possibleNumbers not in possibleNumbersTotal:
-                        # print('possibleNumbers: ' + str(possibleNumbers))
                         possibleNumbersTotal.append(possibleNumbers.copy())
                         numbersToCarryOver = []
                         for j in range(len(possibleNumbers)):
                             if j < possibleNumbers.index(options[i]):
                                 numbersToCarryOver.append(possibleNumbers[j])
-                        # print('numbersToCarryOver: ' + str(numbersToCarryOver))
                         possibleNumbers.clear()
                         for j in range(len(numbersToCarryOver)):
                             possibleNumbers.append(numbersToCarryOver[j])
-                        # print('moved to next number')
                     else:
                         return
     
     function(n)
     
     if possibleNumbersTotal != []:
-        print('possibleNumbersTotal: ' + str(possibleNumbersTotal))
         possibleCounts = []
         for i in range(len(possibleNumbersTotal)):
             possibleCounts.append(len(possibleNumbersTotal[i]))
@@ -57,7 +43,4 @@ def solution(n):
     else:
         numberToReturn = len(possibleNumbers) - 1
         
-    print('numberToReturn: ' + str(numberToReturn))
     return numberToReturn
-
-solution(n)
